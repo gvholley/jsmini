ec647f5476c59514a435dd366f88d10946a1009c
Grant Holley <gvholley@gmail.com>
2020-11-04 21:21:46 +0900

    Completed menu exercise

--- Hamburger/script.js
+++ Hamburger/script.js
@@ -1,7 +1,9 @@
-const btn = document.getElementByID('btn');
-const nav = document.getElementByID('nav');
+const btn = document.getElementById('btn');
+const nav = document.getElementById('nav');
 
 btn.addEventListener('click', () => {
   nav.classList.toggle('active');
   btn.classList.toggle('active');
 });
+
+console.log(btn)
